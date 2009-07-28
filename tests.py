e7139e453c10cc1020a660aee5a7364a88b78aee
alexdej <adejarnatt@gmail.com>
2009-07-27 22:51:49 -0400

    added test stub code

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import puzfile
+
+' this is just a place-holder at this point, need to add real tests
+class BasicTest(unittest.TestCase):
+  def runTest(self):
+   self.assertEqual(1,1) 
+
+if __name__ == '__main__':
+  unittest.main()
