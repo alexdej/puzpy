ebc6101f56c512e6e31139727db3e7a5ac8d7d9b
Alex Dejarnatt <alexdej@vine.co>
2014-02-15 22:27:33 -0500

    add package info

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='puzpy',
+    version='0.1',
+    description='python crossword puzzle library',
+    author='Alex Dejarnatt',
+    author_email='adejarnatt@gmail.com',
+    py_modules=['puz'],
+    install_requires=[]
+)
