673d42621f2a18a6f368d8e7bf2dd205841b6e68
Simeon Visser <simeon87@gmail.com>
2017-09-24 21:28:08 +0100

    Added support for Python 3.6

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Games/Entertainment :: Puzzle Games',
     ],
     keywords='puz crosswords crossword puzzle acrosslite xword xwords'
