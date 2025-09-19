627d636f2575817874ede4213ae9404eb7154077
Alex DeJarnatt <adejarnatt@gmail.com>
2025-09-19 09:43:08 -0400

    Convert README to md format and add a test for code samples in README

---
+++ test_readme.py
@@ -0,0 +1,68 @@
+# # puz.py: python crossword puzzle library (.puz file parser)
+# 
+# Implementation of `.puz` crossword puzzle file parser based on `.puz` file
+# format documentation here:
+# <http://code.google.com/p/puz/wiki/FileFormat>
+# 
+# ## Example Usage
+# 
+def test_line_8():
+    import puz
+    
+    # Load a puzzle file:
+    p = puz.read('testfiles/washpost.puz')
+    
+    # Print clues with answers:
+    numbering = p.clue_numbering()
+    solution = puz.Grid(p.solution, p.width, p.height)
+    
+    print('Across')
+    for clue in numbering.across:
+        answer = solution.get_string_for_clue(clue)
+        print(clue['num'], clue['clue'], '-', answer)
+    
+    print('Down')
+    for clue in numbering.down:
+        answer = solution.get_string_for_clue(clue)
+        print(clue['num'], clue['clue'], '-', answer)
+    
+    # Print the grid:
+    grid = puz.Grid(p.fill, p.width, p.height)
+    for row in range(p.height):
+        print(' '.join(grid.get_row(row)))
+    
+    # Unlock a scrambled solution:
+    p.unlock_solution(7844)
+    
+    # Print the unscrambed solution grid:
+    solution = puz.Grid(p.solution, p.width, p.height)
+    for row in range(p.height):
+        print(' '.join(solution.get_row(row)))
+    
+    # Save a puzzle with modifications:
+    p.fill = 'LAMB' + p.fill[4:]
+    p.save('mine.puz')
+
+
+# ## Notes
+# 
+# The parser is as strict as Across Lite, enforcing internal checksums and
+# magic strings. The parser is designed to round-trip all data in the
+# file, even fields whose utility is unknown. This makes testing easier.
+# It is resilient to garbage at the beginning and end of the file (for
+# example some publishers put the filename on the first line and some
+# files have a rn at the end).
+# 
+# In addition to the handful of tests checked in here, the library has
+# been tested on over 9700 crossword puzzles in .puz format drawn from the
+# archives of several publications including The New York Times, The
+# Washington Post, The Onion, and, the Wall Street Journal. As of writing,
+# it can round-trip 100% of them with full fidelity.
+# 
+# ## Running tests
+# 
+# [python tests.py]{.title-ref}
+# 
+# ## License
+# 
+# MIT License.
